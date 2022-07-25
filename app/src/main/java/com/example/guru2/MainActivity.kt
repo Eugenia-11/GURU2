f72f7591b552bce227770a08a17fcf2db6dabf41
kimgy <kimgy05221@daum.net>
2022-07-25 14:33:18 +0900

    홈 화면

--- app/src/main/java/com/example/guru2/MainActivity.kt
+++ app/src/main/java/com/example/guru2/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.guru2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var cameraButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cameraButton = findViewById(R.id.cameraButton)
+
+        cameraButton.setOnClickListener {
+            var intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
