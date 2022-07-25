ec1e82ab8b0715b588afa91a29413107eccba009
YOO-J <yd30229@naver.com>
2022-07-25 21:11:23 +0900

    link 인원별

--- app/src/main/java/com/example/guru2/MainActivity.kt
+++ app/src/main/java/com/example/guru2/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var cameraButton: Button
     lateinit var button1 : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
