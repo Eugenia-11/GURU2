ec826ccb825a51be8316dcdb3d7d60f10995b92b
kimgy <kimgy05221@daum.net>
2022-07-26 14:14:12 +0900

    카메라 기능

--- app/src/main/java/com/example/guru2/CameraActivity.kt
+++ app/src/main/java/com/example/guru2/CameraActivity.kt
@@ -18,7 +18,7 @@ class CameraActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_camera)
 
 
     }
